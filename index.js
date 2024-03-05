a0edad40e0c30a9b29e3615cfd92e344381ec674
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-03-05 21:35:29 +0200

    allowing to be hosted anywhere

--- index.js
+++ index.js
@@ -94,9 +94,8 @@ app.use(unknownEndpoint)
 
 
 
-const PORT = 3001
-app.listen(PORT , () => {
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
 // console.log('hello world')
