5e7166d15212aaca785b414235f4558f7cb0d4d8
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-03-05 21:08:57 +0200

    EX 3.8: Phonebook backend step 8

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token( 'data' , (request)=> JSON.stringify(request.body))
+app.use(morgan(' :method :url :status :res[content-length] - :response-time ms :data'))
+
+
 
 
 const unknownEndpoint = (request, response) => {
